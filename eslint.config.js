4a9b36450a05adb68c26ae2478f5984669eb301b
chumanic <chumanic@chumanicdeMac-mini.local>
2026-08-08 18:08:42 +0800

    chore: ignore local worktrees in lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   {
-    ignores: ["dist/**", "node_modules/**", "coverage/**"],
+    ignores: ["dist/**", "node_modules/**", "coverage/**", ".worktrees/**"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
